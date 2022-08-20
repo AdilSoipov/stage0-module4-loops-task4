549b40dd9f14af061779d0ef2bff5b6a7753702c
AdilSoipov <111345038+AdilSoipov@users.noreply.github.com>
2022-08-21 00:19:38 +0500

    Update TwoRangesSum.java

--- src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
+++ src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
@@ -3,33 +3,38 @@ package school.mjc.stage0.loops.task4;
 public class TwoRangesSum {
     public void printSumOfTwoRanges(int numberToSkip, int lastInRow) {
         
-	    int skippedSum = 0;
+	
+        int skippedSum = 0;
 	    int countedSum = 0;
-	    if (numberToSkip > lastInRow){
-	        System.out.println("number to skip is bugger then the last");
-	    }
-	    else if(lastInRow < 0) {
-	        
-	        System.out.println("last number in row is negative");
-	    }
-	    else if (lastInRow > numberToSkip){
-	        int i = 0;
-	        int x = numberToSkip;
-	        int y = lastInRow;
-	        while(i < x){
+	   
+	    if (lastInRow > numberToSkip){
+	        for(int i = 0; i <= numberToSkip; i++){
 	            skippedSum += i;
-	            i++;
-	        while(x < y){
-	            countedSum +=x;
-	            x++;
-	        }          
-	            
 	            
 	        }
-	        
-	    }
+	        for(int i = numberToSkip + 1; i <= lastInRow; i++){
+	            countedSum += i;
+	            numberToSkip++;
+	            
+	        }
 	    System.out.println(skippedSum);
 	    System.out.println(countedSum);
+	        
+	    }
+	    else if(numberToSkip > lastInRow){
+	        System.out.println("number to skip is bugger then the last");
+
+	    }
+	    else if(lastInRow < 0){
+	       System.out.println("last number in row is negative");
+
+	        
+	        
+	    
+	        
+	    }
+	        
+	        
 	    
 
 
